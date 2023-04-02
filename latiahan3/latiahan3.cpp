a805058c4a3840baee147305da2a212f2491cb34
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-04-03 11:26:59 +1200

    int main mhs3 array

--- latiahan3/latiahan3.cpp
+++ latiahan3/latiahan3.cpp
@@ -16,3 +16,30 @@ struct Mahasiswa
 };
 
 
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukan NIM : ";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukan Nama : ";
+		cin.getline(mhs[i].nama, 25);
+		cout << "Alamat : ";
+		cout << "\n\tMasukan Desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "Masukan Kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukan Umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n\nNIM = " << mhs[i].NIM;
+		cout << "\nNama = " << mhs[i].nama;
+		cout << "\nDesa = " << mhs[i].alamat.desa;
+		cout << "\nKota = " << mhs[i].alamat.kota;
+		cout << "\nUmur = " << mhs[i].umur;
+	}
+}
