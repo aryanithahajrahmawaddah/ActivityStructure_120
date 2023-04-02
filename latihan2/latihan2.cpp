1029747439b8dc7c86bbbbc7d3b04cf5091461c3
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-04-03 11:05:00 +1200

    int main mhs

--- latihan2/latihan2.cpp
+++ latihan2/latihan2.cpp
@@ -14,3 +14,26 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs.NIM;
+	cout << "Masukan Nama : ";
+	cin >> mhs.nama;
+	cout << "Alamat : ";
+	cout << "\n\tMasukan Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukan Kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukan Umur : ";
+	cin >> mhs.umur;
+
+	cout << "\n\nNIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nKota = " << mhs.alamat.kota;
+	cout << "\nUmur = " << mhs.umur;
+}
