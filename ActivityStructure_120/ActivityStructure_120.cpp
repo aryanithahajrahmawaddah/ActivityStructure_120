190f1495db12580581f0f5777e2d9d1cf88be8b0
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-04-03 10:14:24 +1200

    Add project files.

---
+++ ActivityStructure_120/ActivityStructure_120.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+	string NIM;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+
+int main() {
+	mahasiswa mhs1,mhs2
+
+	mhs1.NIM = "20210140019"
+
+}
